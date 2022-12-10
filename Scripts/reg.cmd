4f21fcc5a1ba0fe60d5e219409ec62d1fc499f5c
zd-dalibor <zd.dalibor@gmail.com>
2022-12-10 02:30:21 +0100

    Cosmetic changes.

---
+++ Scripts/reg.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+SET CONFIGURATION=Debug
+SET SCRIPT_DIR=%~dp0
+
+::
+:: Available modes:
+::    UnregServer        - unrersgister local server
+::    RegServer          - regiser local server
+::    UnregServerPerUser - unregister local server per user
+::    RegServerPerUser   - register local server per user
+::
+SET MODE=RegServerPerUser
+
+SET SERVER=%SCRIPT_DIR%..\%CONFIGURATION%\DriverLicenseReader.exe
+for %%i in ("%SERVER%") do SET "SERVER=%%~fi"
+
+"%SERVER%" /%MODE%
+echo %ErrorLevel%
---
+++ Scripts/reg.ps1
@@ -0,0 +1,14 @@
+$Configuration = 'Debug'
+$ScriptDir = Split-Path -Parent $PSCommandPath
+<#
+Available modes:
+    UnregServer        - unrersgister local server
+    RegServer          - regiser local server
+    UnregServerPerUser - unregister local server per user
+    RegServerPerUser   - register local server per user
+#>
+$Mode = RegServer
+
+$Server = Join-Path "$ScriptDir" "..\$Configuration\DriverLicenseReader.exe" | Resolve-Path
+& $Server /$Mode 2>&1
+Write-Host $LASTEXITCODE
